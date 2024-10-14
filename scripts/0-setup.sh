93cddc1c04783818f0404e98802450b65725b974
Alan Cleary <acleary@ncgr.org>
2024-10-14 14:20:47 -0600

    Updated set up script to add conda environment as Jupyter kernal

--- scripts/0-setup.sh
+++ scripts/0-setup.sh
@@ -1,3 +1,6 @@
+# conda environment name
+CONDA_ENV_NAME="nigms-pangenomics"
+
 # update conda
 conda update -n base -c conda-forge conda -y
 
@@ -7,8 +10,15 @@ conda config --add channels conda-forge
 conda config --set channel_priority strict
 
 # create and activate a conda environment
-conda create -n nigms-pangenomics -y
-conda activate nigms-pangenomics
+conda create -n ${CONDA_ENV_NAME} -y
+conda activate ${CONDA_ENV_NAME}
 
 # install pggb
 conda install -c conda-forge pggb -y
+
+# install other software here...
+
+# add the conda environment as a Jupyter kernel
+conda install ipykernel -y
+DL_ANACONDA_ENV_HOME="${DL_ANACONDA_HOME}/envs/${CONDA_ENV_NAME}"
+python -m ipykernel install --prefix "${DL_ANACONDA_ENV_HOME}" --name ${CONDA_ENV_NAME} --display-name "conda-${CONDA_ENV_NAME}"
