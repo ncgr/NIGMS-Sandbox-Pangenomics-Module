a6b03678a4e0aeadea9313610f7b927479360c24
Alan Cleary <acleary@ncgr.org>
2024-10-14 12:56:31 -0600

    Added set up script
    Currently only sets up  Bioconda and installs pggb.

---
+++ scripts/0-setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# set up Bioconda
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+
+# create and activate a conda environment
+conda create -n nigms-pangenomics -y
+conda activate nigms-pangenomics
+
+# install pggb
+conda install -c bioconda pggb
